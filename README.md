e590d086ec59b62032dfc7456a4af040c4c70184
Elizabeth Benner <ebenner@mail.greenriver.edu>
2017-06-06 12:33:10 -0500

    Added requirements list to the readme

--- README.md
+++ README.md
@@ -1,6 +1,32 @@
 # bark-parks
 Team final project Dog Park Finder
 
+Your website should have the following features:
+
+Separates all database/business logic using the MVC pattern.
+
+Routes all URLs and leverages a templating language using the Fat-Free framework.
+
+Has a clearly defined database layer using PDO and prepared statements.
+
+Data can be viewed, added, updated, and deleted.
+
+Has a history of commits from both team members to a Git repository.
+
+Uses OOP, and defines multiple classes, including at least one inheritance relationship.
+- We used a Park class to create park entries on the website, User class to create users,
+and an AdminUser class which extends User to create administrative users with extra admin permissions.
+
+Contains full Docblocks for all PHP files.
+
+Has full validation on the client side through JavaScript and server side through PHP.
+
+Incorporates jQuery and Ajax.
+
+BONUS:  Utilizes an API
+
+Create Table Statements:
+
 CREATE TABLE parks (
   id INT(3) NOT NULL AUTO_INCREMENT PRIMARY KEY,
   park_name VARCHAR(255),
@@ -32,3 +58,4 @@ CREATE TABLE users (
     username VARCHAR(255),
     password VARCHAR(255)
 );
+
---
+++ tmp/2a0gwkr52akgg.122y7ayggsyck.php
@@ -0,0 +1,73 @@
+<!DOCTYPE html>
+<html>
+	<head>
+	<meta charset="UTF-8">
+	<meta http-equiv="X-UA-Compatible" content="IE=edge">
+	<meta name ="viewport" content="width=device-width, initial-scale=1">
+	<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css">
+	<link rel="stylesheet" href="styles/styles.css">
+	<title>Bark Parks</title>
+	</head>
+    <body>
+        
+        
+     <nav class="navbar navbar-default">
+        <div class="container-fluid">
+         <!-- Brand and toggle get grouped for better mobile display -->
+          <div class="navbar-header">
+             <button type="button" class="navbar-toggle collapsed" data-toggle="collapse" data-target="#bs-example-navbar-collapse-1" aria-expanded="false">
+              <span class="sr-only">Toggle navigation</span>
+              <span class="icon-bar"></span>
+              <span class="icon-bar"></span>
+              <span class="icon-bar"></span>
+             </button>
+              <a class="navbar-brand" href="./">BarkParks</a>
+          </div>
+         <!-- Collect the nav links, forms, and other content for toggling -->
+         <div class="collapse navbar-collapse" id="bs-example-navbar-collapse-1">
+              <ul class="nav navbar-nav">
+                <li class="active"><a href="./">Home <span class="sr-only">(current)</span></a></li>
+                <li><a href="./login">Login</a></li>
+                <li><a href="./newaccount">Create an Account</a></li>
+              </ul>
+        </div><!-- /.container-fluid -->
+        </nav>
+     
+     
+         <div class="container">
+            <div class="col-sm-12 col-md-12 col-xl-12">
+                 <div class="row">
+                 <div class="col-sm-5"></div>
+             <form class="navbar-form col-sm-6" role="search">
+         <div class="input-group add-on">
+              <input class="form-control" placeholder="Search" name="srch-term" id="srch-term" type="text">
+              <div class="input-group-btn">
+               <button class="btn btn-default" type="submit"><i class="glyphicon glyphicon-search"></i></button>
+             </div>
+         </div>
+        </form>
+       </div> <!-- end of row-->
+     </div>
+       <table class="table table-responsive">
+            <thead>
+                <tr>  
+                    <th>Park</th>
+                    <th>Location</th>
+                    <th>Rating</th>
+                    <th>Edit</th>
+                </tr>
+            </thead>
+            <tbody>
+                <tr><!-- Loop for data should begin here-->
+                  <td>ParkName1</td>
+                  <td>ParkLocation1</td>
+                  <td>3 out of 5</td>
+                  <td>Picture of a Pencil.jpeg</td>
+                </tr><!-- end of loop-->
+            </tbody>
+            
+        </table>     
+            
+     </div>   
+    </body>
+</html>
