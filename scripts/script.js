1a5d270bec5c909247ec00209c16321412bd8516
Elizabeth Benner <ebenner@mail.greenriver.edu>
2017-06-07 21:59:03 -0500

    Started putting together rating system on viewpark page

--- scripts/script.js
+++ scripts/script.js
@@ -1,3 +1,36 @@
 $(document).ready(function(){
     $('#parks-table').DataTable();
+    
+    $('.ratings_stars').hover(
+            // Handles the mouseover
+            function() {
+                $(this).prevAll().addBack().addClass('ratings_over');
+                $(this).nextAll().removeClass('ratings_vote'); 
+            },
+            // Handles the mouseout
+            function() {
+                $(this).prevAll().addBack().removeClass('ratings_over');
+                set_votes($(this).parent());
+            }
+        );
+
+    $('.rate_widget').each(function(i) {
+                var widget = this;
+                var out_data = {
+                    widget_id : $(widget).attr('id'),
+                    fetch: 1
+                };
+                $.post(
+                    'ratings.php',
+                    out_data,
+                    function(INFO) {
+                        $(widget).data( 'fsr', INFO );
+                        set_votes(widget);
+                    },
+                    'json'
+                );
+            });
+
+
 });
+
